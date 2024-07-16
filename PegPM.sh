e9e97dc3eedf9b32a14f93a5aa8020b28d996b2e
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-07-16 04:07:53 -0500

    Add files via upload

---
+++ PegPM.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "No name provided"
+    exit 1
+fi
+
+echo "Now installing $1.pmp"
+wget -q --show-progress https://github.com/acesavagejr/PegPM/raw/main/Projects/$1.pmp
+
+if [ $? = 8 ]; then
+clear
+echo -e "\e[96m\e[1mUh Oh..."
+echo -e "\e[0mIt seems that project doesn't exist
+make sure you typed in the name right"
+echo -e "\e[2mRemeber, projects with spaces are replased with dashes"
+exit
+fi
+
+if [ $? = 7 ]; then
+clear
+echo -e "\e[96m\e[1mUh Oh..."
+echo -e "\e[0mIt seems that there was a protocall error"
+exit
+fi
+echo -e "\e[96m\e[1mInstalled!"
+echo -e "\e[0m$1.pmp is now installed in your current derectory"
