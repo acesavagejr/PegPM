d1714e0af34846db2c68a4b116754ab02706d33c
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-07-16 04:28:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ wget https://github.com/acesavagejr/PegPM/raw/main/PegPM.sh
 cd ~/
 echo "alias pegpm='~/.PegPM/PegPM.sh'" >> .bashrc
 echo -e "\e[5mPegPM is now installed!"
-echo
+echo -e "\e[0m"
 chmod u+x ~/.PegPM/PegPM.sh
