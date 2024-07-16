05f25ff3f498048e09613f2a90b81d9108e4effd
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-07-16 04:04:13 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/
+mkdir .PegPM
+cd .PegPM
+wget https://github.com/acesavagejr/PegPM/raw/main/PegPM.sh
+cd ~/
+echo "alias pegpm='~/.PegPM/PegPM.sh'" >> .bashrc
+echo -e "\e[5mPegPM is now installed!"
