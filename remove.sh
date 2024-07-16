c9d8f861a3704cf6df068482a23cd388d87401f0
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-07-16 04:30:50 -0500

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/
-rmdir .PegPM
+rmdir .PegPM --ignore-fail-on-non-empty
 echo "\e[1m\e[31m\e[5mWARNING! \e[25m\e[39mthis part requires sed!"
 sed -i 'alias pegpm='~/.PegPM/PegPM.sh'' .bashrc
 echo "\e[5mPegPM Has been removed"
